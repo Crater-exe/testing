2394ee1c84d2e717f8d50d619bab047b13628eb5
Carter <hanercarter@gmail.com>
2024-10-09 12:22:11 -0300

    lesson

--- src/components/OrdersReport.tsx
+++ src/components/OrdersReport.tsx
@@ -2,11 +2,40 @@
 
 import { getJSONData } from '@/tools/Toolkit';
 import { Orders, Order } from '@/tools/orders.model';
+import { useEffect, useState } from 'react';
 
-export default function OrdersReport() {
+export default function OrdersReport({setAppState, appState}:{setAppState:Function, appState:number}) {
     // retrieve server sided script
     const RETRIEVE_SCRIPT:string = "https://www.seanmorrow.ca/_lessons/retrieveOrder.php";
-  
 
-    return (<>No orders retrieved...</>);
+    // ----------------------------- private methods
+    const getOrders = async () => {
+        const data:Orders = await getJSONData(RETRIEVE_SCRIPT, false, true);
+        console.log(data);
+
+        // save it in state variable - because it is used in JSX and needs to persist
+        setOrders(data.orders);
+
+        // data all loaded!
+        setAppState(3);
+    }
+
+    
+    // ----------------------------- use effects
+    useEffect(() => {
+        if (appState == 2) getOrders();
+    }, [appState]);
+
+    // ----------------------------- state variables
+    const [orders, setOrders] = useState<Order[]>([]);
+  
+    if (appState == 1) {
+        return (<>No orders retrieved...</>);
+    } else if (appState == 3) {
+        return (
+            <>
+                !!! render out orders content here !!!
+            </>
+        );
+    }
 }
